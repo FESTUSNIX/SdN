d657184a34ffa94aee724a70c7d9fd2674915bd4
Festus <matkowsky808@gmail.com>
2023-06-18 16:51:47 +0200

    feat: Create shadcn/ui components

---
+++ app/components/elements/Typography/index.tsx
@@ -0,0 +1,7 @@
+export { H1 } from './H1'
+export { H2 } from './H2'
+export { H3 } from './H3'
+export { H4 } from './H4'
+export { List } from './List'
+export { Muted } from './Muted'
+export { Blockquote } from './Blockquote'
