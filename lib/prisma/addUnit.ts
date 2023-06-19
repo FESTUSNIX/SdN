4ae9457fdee6ca67ea70d06790f9c65b717485ae
Festus <matkowsky808@gmail.com>
2023-06-20 00:43:28 +0200

    feat: Add addUnit utility function

---
+++ lib/prisma/addUnit.ts
@@ -0,0 +1,22 @@
+import { Unit, UnitAddress } from '@prisma/client'
+
+export async function addUnit(
+	unitData: Omit<Unit, 'updatedAt' | 'id'> & { address: Omit<UnitAddress, 'id' | 'unitId'> }
+) {
+	const res = await fetch(`${process.env.BASE_URL ?? ''}/api/addUnit`, {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json'
+		},
+		body: JSON.stringify(unitData)
+	})
+
+	if (!res.ok) {
+		console.log(res)
+		throw new Error('Failed to fetch')
+	}
+
+	const data = await res.json()
+
+	return data
+}
