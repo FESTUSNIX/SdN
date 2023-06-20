0c5e56f7a127ccd24273ec41338c889b0283c548
Festus <matkowsky808@gmail.com>
2023-06-20 17:51:43 +0200

    fix: Vercel fix try

--- lib/utils/getBaseUrl.ts
+++ lib/utils/getBaseUrl.ts
@@ -1,3 +1,5 @@
-export function getBaseUrl() {
-	return process.env.NEXT_PUBLIC_SITE_URL ?? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
+export const getBaseUrl = () => {
+	if (process.env.NEXT_PUBLIC_VERCEL_ENV === 'production') return 'https://sdn-theta.vercel.app'
+	if (process.env.NEXT_PUBLIC_VERCEL_ENV === 'preview') return `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
+	return 'http://localhost:3000'
 }
