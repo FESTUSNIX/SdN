ad591db56565f62ba54e57fade2c80d587a288e6
Festus <matkowsky808@gmail.com>
2023-06-20 17:47:47 +0200

    fix: Vercel fix try

--- lib/utils/getBaseUrl.ts
+++ lib/utils/getBaseUrl.ts
@@ -1,6 +1,3 @@
-// in some util
-export const getBaseUrl = () => {
-	if (process.env.NEXT_PUBLIC_VERCEL_ENV === 'production') return 'https://your-production.url'
-	if (process.env.NEXT_PUBLIC_VERCEL_ENV === 'preview') return `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
-	return 'http://localhost:3000'
+export function getBaseUrl() {
+	return process.env.NEXT_PUBLIC_SITE_URL ?? process.env.NEXT_PUBLIC_VERCEL_URL
 }
