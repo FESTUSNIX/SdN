11bfdb78c39b9d656cd99022b2dd5decc9288c73
Festus <matkowsky808@gmail.com>
2023-06-21 21:34:25 +0200

    feat: Add AdminUnitPage

---
+++ app/admin/components/modules/UnitForm/components/elements/Name/page.tsx
@@ -0,0 +1,37 @@
+import { Major, Unit } from '@prisma/client'
+import React from 'react'
+import { H1 } from '@/app/components/elements/Typography'
+import Image from 'next/image'
+import { urlFor } from '@/lib/supabase/getUrlFor'
+import { getUnit } from '@/lib/prisma/getUnit'
+import { getUnits } from '@/lib/prisma/getUnits'
+
+type Props = { params: { unitId: number } }
+
+export async function generateStaticParams() {
+	const { units } = await getUnits()
+
+	return units?.map((unit: any) => ({
+		unitId: unit.id.toString()
+	}))
+}
+
+const AdminUnitPage = async ({ params }: Props) => {
+	const { unit } = await getUnit(params.unitId)
+	console.log(unit)
+	if (!unit) return null
+
+	return (
+		<main className='flex min-h-screen flex-col items-center wrapper pt-12'>
+			<H1 className='mb-24'>{unit.name}</H1>
+
+			{unit.logo && (
+				<Image src={urlFor('unit_logos', unit.logo).publicUrl} alt={`Logo ${unit.name}`} width={100} height={100} />
+			)}
+
+			<div className='max-w-full break-all'>{JSON.stringify(unit)}</div>
+		</main>
+	)
+}
+
+export default AdminUnitPage
