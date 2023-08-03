779e23c2174937af0d0c0e2bcf7d1c6b826d6342
Festus <matkowsky808@gmail.com>
2023-08-03 22:27:29 +0200

    fix: suppressHydrationWarning

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -15,7 +15,7 @@ export const metadata = {
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
 	return (
-		<html lang='en'>
+		<html lang='en' suppressHydrationWarning>
 			<body className={cn(inter.className, 'flex h-full flex-col')}>
 				<Providers>
 					<Navbar />
