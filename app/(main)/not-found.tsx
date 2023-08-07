2405f2edffaf1fe9cb4bdc1ff8a27ae43e519d57
Festus <matkowsky808@gmail.com>
2023-08-07 22:56:45 +0200

    feat: Add global not-found page

---
+++ app/(main)/not-found.tsx
@@ -0,0 +1,22 @@
+import Link from 'next/link'
+import { buttonVariants } from '../components/ui/Button'
+import { cn } from '@/lib/utils/utils'
+
+export default function NotFound() {
+	return (
+		<div className='wrapper my-12 flex flex-col items-center md:my-24'>
+			<h1 className='mb-4 text-8xl font-black md:text-9xl'>404</h1>
+			<h2 className='text-center text-5xl font-bold md:text-6xl'>Strona nie odnaleziona</h2>
+			<p className='mt-2 text-center text-xl'>Nie udało się znaleźć żądanego zasobu</p>
+
+			<div className='mt-12 flex w-full flex-col items-center gap-2 sm:flex-row sm:justify-center'>
+				<Link href='/contact' className={cn(buttonVariants({ size: 'lg', variant: 'secondary' }), 'max-sm:w-full')}>
+					Skontaktuj się z nami
+				</Link>
+				<Link href='/' className={cn(buttonVariants({ size: 'lg' }), 'max-sm:w-full')}>
+					Strona główna
+				</Link>
+			</div>
+		</div>
+	)
+}
