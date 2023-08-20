9ef8b16f445ab1eee0d423aac2e12c879d5395d3
Festus <matkowsky808@gmail.com>
2023-08-20 20:49:20 +0200

    feat: Add placeholder image

--- app/(main)/units/[unitSlug]/components/Header/index.tsx
+++ app/(main)/units/[unitSlug]/components/Header/index.tsx
@@ -2,6 +2,7 @@ import { Badge } from '@/app/components/ui/Badge'
 import { H1, Muted } from '@/app/components/ui/Typography'
 import { urlFor } from '@/lib/supabase/getUrlFor'
 import { capitalize } from '@/lib/utils/capitalize'
+import { placeholderImage } from '@/lib/utils/placeholderImage'
 import { Unit } from '@prisma/client'
 import { LinkIcon, MapPin } from 'lucide-react'
 import Image from 'next/image'
@@ -12,16 +13,16 @@ type Props = Pick<Unit, 'name' | 'logo' | 'unitType' | 'isPublic' | 'website' | 
 	voivodeship: string
 }
 
-const Header = ({ isPublic, logo, name, unitType, website, city, voivodeship, updatedAt }: Props) => {
+const Header = ({ isPublic, logo, name, unitType, website, city, voivodeship }: Props) => {
 	return (
 		<header className='mt-12 flex flex-col gap-6 border-b py-6 lg:flex-row'>
 			<div className='mx-auto grid w-full max-w-full place-items-center overflow-hidden rounded-lg border bg-muted sm:max-w-[280px] md:mx-0 md:w-auto'>
 				<Image
-					src={logo ? urlFor('unit_logos', logo).publicUrl : ''}
+					src={logo ? urlFor('unit_logos', logo).publicUrl : placeholderImage}
 					alt={`Logo ${name}`}
 					width={400}
 					height={400}
-					className='aspect-square h-full w-full max-w-[280px]'
+					className='aspect-square h-full w-full max-w-[280px] object-cover'
 				/>
 			</div>
 
