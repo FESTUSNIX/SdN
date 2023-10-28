4d7e9954833a818db27192a5ccbae658d6b3323b
Festus <matkowsky808@gmail.com>
2023-10-29 00:17:47 +0200

    feat: Add go back button

--- app/(manage)/manage/majors/[slug]/page.tsx
+++ app/(manage)/manage/majors/[slug]/page.tsx
@@ -1,7 +1,11 @@
+import { buttonVariants } from '@/app/components/ui/Button'
 import { Separator } from '@/app/components/ui/Separator/separator'
 import { H1 } from '@/app/components/ui/Typography'
 import { getAuthSession } from '@/lib/auth/auth'
+import { cn } from '@/lib/utils/utils'
 import prisma from '@/prisma/client'
+import { ArrowLeft } from 'lucide-react'
+import Link from 'next/link'
 import { redirect } from 'next/navigation'
 import PreviewMajorData from './components/PreviewMajorData'
 
@@ -35,6 +39,13 @@ export default async function ManageMajorPage({ params: { slug } }: { params: { 
 	return (
 		<div className='flex h-full flex-col gap-8'>
 			<section>
+				<Link
+					href={'/manage/majors'}
+					className={cn(buttonVariants({ variant: 'link' }), 'mb-4 gap-2 pl-0')}>
+					<ArrowLeft className='h-4 w-4' />
+					<span>Powrót do listy</span>
+				</Link>
+
 				<H1>{major.name}</H1>
 
 				<Separator className='mt-4' />
