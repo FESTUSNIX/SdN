9d96d7b05a14d1c7d1e86cbe8aeb9067b07c82dd
Festus <matkowsky808@gmail.com>
2023-10-31 22:30:51 +0100

    feat: Add search bar for majors

---
+++ app/(manage)/manage/majors/components/SearchBar.tsx
@@ -0,0 +1,46 @@
+'use client'
+
+import { Input } from '@/app/components/ui/Input'
+import { useDebounce } from '@/app/hooks/useDebounce'
+import { Search as SearchIcon } from 'lucide-react'
+import { usePathname, useRouter, useSearchParams } from 'next/navigation'
+import { ChangeEvent, useCallback, useEffect, useState } from 'react'
+
+export const SearchBar = () => {
+	const router = useRouter()
+	const pathname = usePathname()
+	const searchParams = useSearchParams()!
+
+	const [query, setQuery] = useState('')
+	const debouncedQuery = useDebounce(query, 500)
+
+	const createQueryString = useCallback(
+		(name: string, value: string) => {
+			const params = new URLSearchParams(searchParams)
+			params.set(name, value)
+
+			return params.toString()
+		},
+		[searchParams]
+	)
+
+	const handleChange = (event: ChangeEvent<HTMLInputElement>) => setQuery(event.target.value)
+
+	useEffect(() => {
+		if (!query) return router.push(pathname)
+
+		router.push(pathname + '?' + createQueryString('q', query))
+	}, [debouncedQuery])
+
+	return (
+		<div className='relative'>
+			<SearchIcon className='pointer-events-none absolute left-4 top-1/2 z-10 h-4 w-4 -translate-y-1/2 select-none text-muted-foreground' />
+			<Input
+				placeholder='Wyszukaj kierunki'
+				className='rounded-full bg-secondary py-4 pl-12 pr-6'
+				value={query}
+				onChange={handleChange}
+			/>
+		</div>
+	)
+}
--- app/(manage)/manage/majors/page.tsx
+++ app/(manage)/manage/majors/page.tsx
@@ -3,15 +3,26 @@ import { H1, Muted } from '@/app/components/ui/Typography'
 import { getAuthSession } from '@/lib/auth/auth'
 import prisma from '@/prisma/client'
 import { redirect } from 'next/navigation'
-import MajorCard from './components/MajorCard'
 import AddMajor from './components/AddMajor'
+import MajorCard from './components/MajorCard'
+import { SearchBar } from './components/SearchBar'
 
-export default async function ManageMajorsPage() {
+export default async function ManageMajorsPage({
+	searchParams
+}: {
+	searchParams: { [key: string]: string | string[] | undefined }
+}) {
 	const session = await getAuthSession()
 	if (!session) redirect('/login')
 
+	const searchQuery = searchParams.q?.toString().replace(/[\s\n\t]/g, '_')
+
 	const majors = await prisma.major.findMany({
 		where: {
+			name: {
+				contains: searchQuery,
+				mode: 'insensitive'
+			},
 			unit: {
 				managers: {
 					some: {
@@ -29,13 +40,6 @@ export default async function ManageMajorsPage() {
 			isOnline: true,
 			majorLevel: true,
 			slug: true,
-			unit: {
-				select: {
-					id: true,
-					name: true,
-					slug: true
-				}
-			},
 			qualifications: {
 				select: {
 					slug: true,
@@ -45,10 +49,23 @@ export default async function ManageMajorsPage() {
 		}
 	})
 
+	const unit = await prisma.unit.findFirst({
+		where: {
+			managers: {
+				some: {
+					id: session.user.id
+				}
+			}
+		},
+		select: {
+			id: true
+		}
+	})
+
 	if (!majors) return <div>Nie odnaleziono żadnych kierunków</div>
 
 	return (
-		<div className='flex h-full flex-col gap-8'>
+		<div className='flex h-full flex-col gap-8 px-1'>
 			<section>
 				<H1>Kierunki</H1>
 				<Muted className='text-base'>Zarządzaj kierunkami jednostki</Muted>
@@ -56,10 +73,12 @@ export default async function ManageMajorsPage() {
 				<Separator className='mt-4' />
 			</section>
 
-			<section className='flex items-center'>
-				<div className='ml-auto'>
-					<AddMajor unitId={majors[0].unit.id}  />
+			<section className='flex flex-col justify-between gap-x-6 gap-y-4 sm:flex-row lg:items-center lg:gap-x-12'>
+				<div className='grow'>
+					<SearchBar />
 				</div>
+
+				<div className='ml-auto'>{unit && <AddMajor unitId={unit.id} />}</div>
 			</section>
 
 			<section className='space-y-2 pb-6'>
