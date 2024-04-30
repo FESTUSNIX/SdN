22577510746893f847f1ef7a1737c111c4091353
Festus <matkowsky808@gmail.com>
2024-04-30 13:28:52 +0200

    refractor: Remove unncecessary dependency

--- lib/supabase/uploadImage.ts
+++ lib/supabase/uploadImage.ts
@@ -1,10 +1,17 @@
 'use client'
 
-import { v4 as uuidv4 } from 'uuid'
 import { supabase } from './supabase'
 
+const getUUID = () => {
+	return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, c => {
+		const r = (Math.random() * 16) | 0
+		const v = c === 'x' ? r : (r & 0x3) | 0x8
+		return v.toString(16)
+	})
+}
+
 export const uploadFileToSupabase = async (bucket: string, file: File, filename?: string) => {
-	const _filename = filename ?? `${uuidv4()}-${file.name}`
+	const _filename = filename ?? `${getUUID()}-${file.name}`
 
 	const { data, error } = await supabase.storage.from(bucket).upload(`${_filename}`, file, {
 		cacheControl: '36000',
