19ca1a740237b4399ffbc58bcdf2a1ffe76dd107
Festus <matkowsky808@gmail.com>
2024-08-24 01:09:28 +0200

    feat: Add dynamic promoted majors

--- app/(main)/components/sections/RecommendedMajors.tsx
+++ app/(main)/components/sections/RecommendedMajors.tsx
@@ -1,39 +1,64 @@
 import MajorCard from '@/app/components/Majors/Card'
 import prisma from '@/prisma/client'
+import { unstable_cache } from 'next/cache'
 
 type Props = {}
 
-export const RecommendedMajors = async (props: Props) => {
-	const majors = await prisma.major.findMany({
-		take: 8,
-		where: {
-			status: 'PUBLISHED'
-		},
-		select: {
-			id: true,
-			name: true,
-			majorLevel: true,
-			isOnline: true,
-			slug: true,
-			qualifications: {
-				select: {
-					slug: true,
-					name: true
+const getCachedPromotedMajors = unstable_cache(
+	async () => {
+		return await prisma.promotedMajors.findMany({
+			select: {
+				major: {
+					select: {
+						id: true,
+						name: true,
+						majorLevel: true,
+						isOnline: true,
+						slug: true,
+						qualifications: {
+							select: {
+								slug: true,
+								name: true
+							}
+						},
+						unit: {
+							select: {
+								name: true
+							}
+						}
+					}
 				}
 			},
-			unit: {
-				select: {
-					name: true
-				}
+			take: 8,
+			orderBy: {
+				promotedAt: 'desc'
 			}
-		}
-	})
+		})
+	},
+	[],
+	{
+		tags: ['promotedMajors'],
+		// 2 hours
+		revalidate: 60 * 60 * 2
+	}
+)
+
+export const RecommendedMajors = async (props: Props) => {
+	const promotedMajors = await getCachedPromotedMajors()
+
+	if (!promotedMajors.length) return null
+
+	const majors = promotedMajors.map(promotedMajor => promotedMajor.major)
 
 	return (
 		<section className='wrapper mb-32'>
-			<h2 className='mx-auto mb-6 text-center font-heading text-3xl font-semibold sm:mb-8 sm:text-4xl md:mb-12 md:text-5xl'>
-				Nasze rekomendacje
-			</h2>
+			<div className='mb-12'>
+				<h2 className='mb-2 font-heading text-3xl font-semibold sm:text-4xl md:text-5xl'>Nasze rekomendacje</h2>
+				<p className='max-w-prose text-pretty text-base text-muted-foreground'>
+					Staramy się wybierać dla Ciebie najlepsze kierunki studiów. Zobacz, które kierunki są teraz najbardziej
+					popularne i cieszą się największym zainteresowaniem. Może znajdziesz coś dla siebie?
+				</p>
+			</div>
 
 			<div className='grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4'>
 				{majors.map(major => (
